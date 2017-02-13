5c6cde6383bc9759a5d665a200b6cb268c5daf51
Niklas Barning <niklas@barning.org>
2017-02-13 21:52:24 +0100

    First try to set a status

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var dotenv = require('dotenv').config();
 var SlackBot = require('slackbots');
 var low = require('lowdb')
-var schedule = require('node-schedule');
 
 var db = low('db.json');
 
@@ -20,10 +19,6 @@ var bot = new SlackBot({
 bot.on('start', function() {
   db.defaults({ absence: []})
   .value()
-});
-
-var absenceStatusPost = schedule.scheduleJob('0	10	*	*	*	', function(){
-  bot.postMessageToChannel('abwesenheitsliste', 'Guten Morgen, diese Personen sind abwesend: ' + returnAbsenceList(), params);
 });
 
 function setListener(user) {
@@ -120,6 +115,46 @@ function setOnline(_user, _channel) {
   }
 }
 
+function otherUserMention(d) {
+  var json = bot.getUsers()._value.members;
+  var mention = { bool:false, name: undefined };
+
+  for(var i = 0; i < json.length; i++) {
+    var name = json[i].name;
+    console.log(json[i], 'frommention');
+    if (findWord(name,d)) {
+      console.log('true');
+      mention.bool= true;
+      mention.name= name;
+    }
+    else {
+      mention.bool = false;
+      console.log('false');
+    }
+
+    return mention;
+  }
+}
+
+function findKeyWords(msg, channel, user) {
+  if (findWord('abwesend',msg) == true){
+    setAway(user,channel);
+  }
+
+  if (findWord('anwesend',msg) == true) {
+    setOnline(user,channel);
+  }
+
+  if (findWord('status',msg) == true){
+    checkUserStatus(channel);
+  }
+
+  if ( findWord('status',msg) == false && findWord('anwesend',msg) == false && findWord('abwesend',msg) == false) {
+    console.log('Error in channel ' + channel + ' with user ' + user);
+    bot.postMessage(channel, 'hey, ' + user + ' ich habe dich nicht verstanden. Du kannst mir die Befehle "anwesend", "abwesend", oder "status" geben.', params);
+  }
+}
+
 function postChannel(d){
   var channel = d.channel;
   if (d.text) {
@@ -129,25 +164,13 @@ function postChannel(d){
     var msg= d.content;
   }
   if (msg){
-    var user = checkUser(d.user);
-
-    if (findWord('abwesend',msg) == true){
-      setAway(user,channel);
+    if (otherUserMention(msg).bool) {
+      console.log('otheruser');
+      findKeyWords(msg, channel, otherUserMention(msg).name);
+    } else {
+      var user = checkUser(d.user);
+      findKeyWords(msg, channel, user);
     }
-
-    if (findWord('anwesend',msg) == true) {
-      setOnline(user,channel);
-    }
-
-    if (findWord('status',msg) == true){
-      checkUserStatus(channel);
-    }
-
-    if ( findWord('status',msg) == false && findWord('anwesend',msg) == false && findWord('abwesend',msg) == false) {
-      console.log('Error in channel ' + channel + ' with user ' + user);
-      bot.postMessage(channel, 'hey, ' + user + ' ich habe dich nicht verstanden. Du kannst mir die Befehle "anwesend", "abwesend", oder "status" geben.', params);
-    }
-
   }
 }
 
